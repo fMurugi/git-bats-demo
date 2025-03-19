dfd2ad31ce6b8d8fb4edf3be1b47c60db03d6d74
fmurugi <fionamurugi99@gmail.com>
2025-03-19 13:28:29 +0300

    extract the tar file and count the file with success and failure in it

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/bash
-#
-echo "There were 78 successes and 22 failures."
+
+#extracts content of a tar file specified as the first command line
+#argument and counts the number of files with word success and files with word failure
+
+tar_file=$1
+
+
+SCRATCH=`mktemp --directory`
+
+# echo $SCRATCH
+
+tar zxf $tar_file --directory $SCRATCH
+
+num_success=`grep -lr SUCCESS $SCRATCH | wc -l`
+num_failures=`grep -lr FAILURE $SCRATCH | wc -l`
+
+echo "There were $num_success successes and $num_failures failures."
+
+rm -rf $SCRATCH
